154d3cae49c0521bcb8d085489348420bbadf2d1
Charliecos250606 <otubocharles2006@gmail.com>
2026-01-14 03:07:46 +0000

    Add files via upload

---
+++ question.js
@@ -0,0 +1,416 @@
+const questionnaire = {
+  "Values and Goal Alignment": [
+    {
+      id: 1,
+      question:
+        "I have clear defined personal and business goals for the next 6-12 months.",
+      options: ["Really/very often", "Mostly", "Sometimes", "Rarely/Never"],
+    },
+    {
+      id: 2,
+      question: "My daily activities reflects my long-term priorities.",
+      options: ["Really/very often", "Mostly", "Sometimes", "Rarely/Never"],
+    },
+    {
+      id: 3,
+      question: "I review and adjust my goals regularly to stay on track.",
+      options: ["Really/very often", "Mostly", "Sometimes", "Rarely/Never"],
+    },
+    {
+      id: 4,
+      question: "My decisions aligns with my core values.",
+      options: ["Really/very often", "Mostly", "Sometimes", "Rarely/Never"],
+    },
+    {
+      id: 5,
+      question:
+        "I feel fulfilled and motivated by the goals I am working towards.",
+      options: ["Really/very often", "Mostly", "Sometimes", "Rarely/Never"],
+    },
+    {
+      id: 6,
+      question: "I set goals that challenge and inspire me to grow.",
+      options: ["Really/very often", "Mostly", "Sometimes", "Rarely/Never"],
+    },
+    {
+      id: 7,
+      question:
+        "I prioritize tasks that aligns with my goals over trivial ones.",
+      options: ["Really/very often", "Mostly", "Sometimes", "Rarely/Never"],
+    },
+    {
+      id: 8,
+      question: "I avoid procrastination by creating actionable plans.",
+      options: ["Really/very often", "Mostly", "Sometimes", "Rarely/Never"],
+    },
+    {
+      id: 9,
+      question: "I communicate my goals effectively to those around me. ",
+      options: ["Really/very often", "Mostly", "Sometimes", "Rarely/Never"],
+    },
+    {
+      id: 10,
+      question: "I track my progress and celebrate milestones along the way.",
+      options: ["Really/very often", "Mostly", "Sometimes", "Rarely/Never"],
+    },
+  ],
+
+  "Financial Productivity and Discipline": [
+    {
+      id: 1,
+      question: "I review my financial health atleast quarterly.",
+      options: ["Really/very often", "Mostly", "Sometimes", "Rarely/Never"],
+    },
+    {
+      id: 2,
+      question: "I have a written budget that I follow consistently.",
+      options: ["Really/very often", "Mostly", "Sometimes", "Rarely/Never"],
+    },
+    {
+      id: 3,
+      question: "I track my income, expenses, and financial growth regularly.",
+      options: ["Really/very often", "Mostly", "Sometimes", "Rarely/Never"],
+    },
+    {
+      id: 4,
+      question: "I have clear financial goals for the next 6-12 months.",
+      options: ["Really/very often", "Mostly", "Sometimes", "Rarely/Never"],
+    },
+    {
+      id: 5,
+      question: "I consistently save or invest a portion of my income.",
+      options: ["Really/very often", "Mostly", "Sometimes", "Rarely/Never"],
+    },
+    {
+      id: 6,
+      question: "I avoid impulse spending and stick to financial priorities.",
+      options: ["Really/very often", "Mostly", "Sometimes", "Rarely/Never"],
+    },
+    {
+      id: 7,
+      question: "I feel confident about my financial management skills.",
+      options: ["Really/very often", "Mostly", "Sometimes", "Rarely/Never"],
+    },
+    {
+      id: 8,
+      question: "I have an emergency fund covering 6-12 months of expenses.",
+      options: ["Really/very often", "Mostly", "Sometimes", "Rarely/Never"],
+    },
+    {
+      id: 9,
+      question: "I make informed decisions about debt and credit.",
+      options: ["Really/very often", "Mostly", "Sometimes", "Rarely/Never"],
+    },
+    {
+      id: 10,
+      question: "I allocate resources towards personal and business growth.",
+      options: ["Really/very often", "Mostly", "Sometimes", "Rarely/Never"],
+    },
+  ],
+
+  "Personal Productivity and Effectiveness": [
+    {
+      id: 1,
+      question: "I create a daily to-do-list to organize my tasks.",
+      options: ["Really/very often", "Mostly", "Sometimes", "Rarely/Never"],
+    },
+    {
+      id: 2,
+      question: "I consistently complete my top priorities for the day.",
+      options: ["Really/very often", "Mostly", "Sometimes", "Rarely/Never"],
+    },
+    {
+      id: 3,
+      question: "I limit distractions when working on import tasks.",
+      options: ["Really/very often", "Mostly", "Sometimes", "Rarely/Never"],
+    },
+    {
+      id: 4,
+      question: "I manage my time effectively and avoid unneccessary delays.",
+      options: ["Really/very often", "Mostly", "Sometimes", "Rarely/Never"],
+    },
+    {
+      id: 5,
+      question: "I delegate tasks whenwhen approprite to maximize efficiency",
+      options: ["Really/very often", "Mostly", "Sometimes", "Rarely/Never"],
+    },
+    {
+      id: 6,
+      question: "I feel in control of my schedule and workload.",
+      options: ["Really/very often", "Mostly", "Sometimes", "Rarely/Never"],
+    },
+    {
+      id: 7,
+      question: "I regularly review and refine my productivity strategies.",
+      options: ["Really/very often", "Mostly", "Sometimes", "Rarely/Never"],
+    },
+    {
+      id: 8,
+      question: "I set boundaries to maintain focus and reduce stress.",
+      options: ["Really/very often", "Mostly", "Sometimes", "Rarely/Never"],
+    },
+    {
+      id: 9,
+      question: "I take breaks to refresh and sustain my productivity.",
+      options: ["Really/very often", "Mostly", "Sometimes", "Rarely/Never"],
+    },
+    {
+      id: 10,
+      question: "I use tools and systerms to stay organizedand on track.",
+      options: ["Really/very often", "Mostly", "Sometimes", "Rarely/Never"],
+    },
+  ],
+
+  "Mindset and Growth": [
+    {
+      id: 1,
+      question: "I believe in my ability to overcome challenges and grow.",
+      options: ["Really/very often", "Mostly", "Sometimes", "Rarely/Never"],
+    },
+    {
+      id: 2,
+      question:
+        "I consistently seek opportunities to learn and develop new skills.",
+      options: ["Really/very often", "Mostly", "Sometimes", "Rarely/Never"],
+    },
+    {
+      id: 3,
+      question: "I embrace feedbacks and use it to improve myself.",
+      options: ["Really/very often", "Mostly", "Sometimes", "Rarely/Never"],
+    },
+    {
+      id: 4,
+      question: "I maintain a positive attitude even during setbacks.",
+      options: ["Really/very often", "Mostly", "Sometimes", "Rarely/Never"],
+    },
+    {
+      id: 5,
+      question: "I surroound myself with supportive and inspiring individuals.",
+      options: ["Really/very often", "Mostly", "Sometimes", "Rarely/Never"],
+    },
+    {
+      id: 6,
+      question: "I practice gratitude and celebrate small wins.",
+      options: ["Really/very often", "Mostly", "Sometimes", "Rarely/Never"],
+    },
+    {
+      id: 7,
+      question: "I challenge limiting beliefs that hinder my growth.",
+      options: ["Really/very often", "Mostly", "Sometimes", "Rarely/Never"],
+    },
+    {
+      id: 8,
+      question: "I invest in personal development programs or resources.",
+      options: ["Really/very often", "Mostly", "Sometimes", "Rarely/Never"],
+    },
+    {
+      id: 9,
+      question: "I reflect on my experiences to gain insights and improve.",
+      options: ["Really/very often", "Mostly", "Sometimes", "Rarely/Never"],
+    },
+    {
+      id: 10,
+      question: "I take responsibility for my actions and outcomes.",
+      options: ["Really/very often", "Mostly", "Sometimes", "Rarely/Never"],
+    },
+  ],
+
+  "Results and Performance": [
+    {
+      id: 1,
+      question:
+        "I consistently meet or exceed my personal and professional goals.",
+      options: ["Really/very often", "Mostly", "Sometimes", "Rarely/Never"],
+    },
+    {
+      id: 2,
+      question: "I track and measure my progress towards desired outcomes.",
+      options: ["Really/very often", "Mostly", "Sometimes", "Rarely/Never"],
+    },
+    {
+      id: 3,
+      question: "I take proactive steps to improve my performance.",
+      options: ["Really/very often", "Mostly", "Sometimes", "Rarely/Never"],
+    },
+    {
+      id: 4,
+      question: "I seek feedbacks to identify areas for improvement.",
+      options: ["Really/very often", "Mostly", "Sometimes", "Rarely/Never"],
+    },
+    {
+      id: 5,
+      question: "I stay focused and committed to achieving results.",
+      options: ["Really/very often", "Mostly", "Sometimes", "Rarely/Never"],
+    },
+    {
+      id: 6,
+      question: "I celebrate results and learn from failures.",
+      options: ["Really/very often", "Mostly", "Sometimes", "Rarely/Never"],
+    },
+    {
+      id: 7,
+      question: "I balance quality and quantity in my work outputs.",
+      options: ["Really/very often", "Mostly", "Sometimes", "Rarely/Never"],
+    },
+    {
+      id: 8,
+      question: "I maintain a results-oriented mindset in my daily activities.",
+      options: ["Really/very often", "Mostly", "Sometimes", "Rarely/Never"],
+    },
+    {
+      id: 9,
+      question: "I adjust my strategies when they are not yielding results.",
+      options: ["Really/very often", "Mostly", "Sometimes", "Rarely/Never"],
+    },
+    {
+      id: 10,
+      question:
+        "I strive for excellence while maintaining ralistic expectations.",
+      options: ["Really/very often", "Mostly", "Sometimes", "Rarely/Never"],
+    },
+  ],
+
+  "Work-Life Balance and Well-being": [
+    {
+      id: 1,
+      question: "I dedicate time to relax and recharge regularly.",
+      options: ["Really/very often", "Mostly", "Sometimes", "Rarely/Never"],
+    },
+    {
+      id: 2,
+      question: "I maintain a healthy balance between work and personal life.",
+      options: ["Really/very often", "Mostly", "Sometimes", "Rarely/Never"],
+    },
+    {
+      id: 3,
+      question: "I prioritize self-care activities such as exercise and sleep.",
+      options: ["Really/very often", "Mostly", "Sometimes", "Rarely/Never"],
+    },
+    {
+      id: 4,
+      question: "I set boundaries to avoid overworking or burnout.",
+      options: ["Really/very often", "Mostly", "Sometimes", "Rarely/Never"],
+    },
+    {
+      id: 5,
+      question: "I spend quality time with family and loved ones.",
+      options: ["Really/very often", "Mostly", "Sometimes", "Rarely/Never"],
+    },
+    {
+      id: 6,
+      question: "I engage in hobbies or activities that brings me joy.",
+      options: ["Really/very often", "Mostly", "Sometimes", "Rarely/Never"],
+    },
+    {
+      id: 7,
+      question:
+        "I manage stress effectively through healthy coping strategies.",
+      options: ["Really/very often", "Mostly", "Sometimes", "Rarely/Never"],
+    },
+    {
+      id: 8,
+      question: "I seek help or support when feeling overwhelmed.",
+      options: ["Really/very often", "Mostly", "Sometimes", "Rarely/Never"],
+    },
+    {
+      id: 9,
+      question: "I reflect on my well-being and make adjustments as needed.",
+      options: ["Really/very often", "Mostly", "Sometimes", "Rarely/Never"],
+    },
+    {
+      id: 10,
+      question: "I feel satisfied with the overall balance in my life.",
+      options: ["Really/very often", "Mostly", "Sometimes", "Rarely/Never"],
+    },
+  ],
+};
+
+const form = document.getElementById("form");
+const category = Object.keys(questionnaire);
+let currentIndex = 0;
+
+function renderingIndex() {
+  const categoryName = category[currentIndex];
+  const h3 = document.createElement("h3");
+  h3.textContent = categoryName.toUpperCase();
+  form.appendChild(h3);
+
+  for (let questions of questionnaire[categoryName]) {
+    const p = document.createElement("p");
+    p.className = "question";
+    p.textContent = `${questions.id}. ${questions.question}`;
+    form.appendChild(p);
+
+    for (let j = 1; j <= 4; j++) {
+      const label = document.createElement("label");
+      label.classList = "option";
+      const input = document.createElement("input");
+      input.type = "radio";
+      input.value = j;
+      input.name = `${questionnaire[categoryName]}_${questions.id}`;
+      label.appendChild(input);
+      label.append(`${questions.options[j - 1]}`);
+      form.appendChild(label);
+    }
+  }
+  if (currentIndex === 0) {
+    prevBtn.style.display = "none";
+  } else {
+    prevBtn.style.display = "inline-block";
+  }
+  if (currentIndex === category.length - 1) {
+    nextBtn.style.display = "none";
+    calculateBtn.style.display = "inline-block";
+  } else {
+    nextBtn.style.display = "inline-block";
+    calculateBtn.style.display = "none";
+  }
+  const progressRead = document.querySelector(".progressRead");
+  const progressValue = document.querySelector(".progressValue");
+  progressRead.style.width = currentIndex * (100 / (category.length - 1)) + "%";
+  console.log(currentIndex * (100 / (category.length - 1)) + "%");
+  progressValue.textContent =
+    currentIndex * (100 / (category.length - 1)) + "%";
+}
+
+prevBtn.addEventListener("click", moveToPrev);
+nextBtn.addEventListener("click", moveToNext);
+
+renderingIndex();
+
+function moveToNext() {
+  if (currentIndex < category.length) {
+    form.innerHTML = "";
+    currentIndex = currentIndex + 1;
+    renderingIndex();
+  }
+}
+function moveToPrev() {
+  if (currentIndex > 0) {
+    form.innerHTML = "";
+    currentIndex = currentIndex - 1;
+    renderingIndex();
+  }
+}
+
+//calculating score
+let answers = [];
+let score = 0;
+function getScore(event) {
+  if (event.target.type === "radio") {
+    const selectedInput = event.target;
+    const questionKey = selectedInput.name;
+    answers[questionKey] = Number(selectedInput.value);
+  }
+  if (answers[questionKey]) {
+    score -= answers[questionKey];
+  }
+  answers[questionKey] = value;
+  score += value;
+  console.log(answers[questionKey]);
+  console.log("Current Score:", score);
+}
+
+document.addEventListener("change", getScore);
+const total = document.getElementById("score");
+total.textContent = score;
